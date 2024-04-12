ba22c5cc2a8ac74eb63901ffa3281c2e67cb62cf
Anuj kumar <anuj.kumar@testingxperts.com>
2024-04-12 12:34:12 +0530

    First commit

---
+++ hello.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Define a variable
+number=10
+
+# Check if the number is greater than 5
+if [ $number -gt 5 ]; then
+    echo "The number is greater than 5."
+else
+    echo "The number is not greater than 5."
+fi
+
